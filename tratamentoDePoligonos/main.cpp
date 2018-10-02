ba61e9fcfdc9bc888f8dfe4a4f758bacf572e911
Mateus de Assis Silva <33188461+TheAssis@users.noreply.github.com>
2018-10-02 16:46:21 -0300

    Algumas atualizações a fim de debug
    Um novo polígono foi inserido, e chamado de trianguloDeitado2, embora seja um retângulo. Apenas o primeiro ponto desse ente geométrico foi rotacionado corretamente, e o segundo ponto está sendo utilizado para debug.

--- tratamentoDePoligonos/main.cpp
+++ tratamentoDePoligonos/main.cpp
@@ -20,17 +20,35 @@ int main (void) {
     cout << "Coordenadas dos vertices do triangulo transladado: \n";
     triangulo.imprime();
 
-    // as linhas a seguir visam testar o metodo da rotaçao
+    cout<<"Agora mandando de volta pro lugar original: \n";
+    triangulo.translada(3,1);
+    triangulo.imprime();
+
+
+    // as linhas a seguir visam testar o metodo da rotaçao em relação a origem. Deu certo
     Poligono trianguloDeitado;
-    trianguloDeitado.insereVertice(1,1);
-    trianguloDeitado.insereVertice(1,10);
-    trianguloDeitado.insereVertice(10,4.5);
+    trianguloDeitado.insereVertice(10,-1);
+    trianguloDeitado.insereVertice(10,1);
+    trianguloDeitado.insereVertice(-10,1);
+    trianguloDeitado.insereVertice(-10,-1);
     cout<<endl<<endl;
-    cout<<"Triangulo antes de rotacionar: \n";
+    cout<<"Triangulo antes de rotacionar em relacao a origem: \n";
     trianguloDeitado.imprime();
-    trianguloDeitado.rotaciona(4.5,4.5,90); // rotacionar 90° em relaçao a origem
+    trianguloDeitado.rotaciona(0,0,90); // rotacionar 90° em relaçao a origem
     cout<<"Triangulo apos rotacao: \n";
     trianguloDeitado.imprime();
 
+    //as linhas a seguir visam testar o metodo de rotacao em relacao a um ponto fora da origem (5,0).
+    Poligono trianguloDeitado2;
+    trianguloDeitado2.insereVertice(0,-1);
+    trianguloDeitado2.insereVertice(0,1);
+    trianguloDeitado2.insereVertice(10,1);
+    trianguloDeitado2.insereVertice(10,-1);
+    cout<<endl<<endl;
+    cout<<"Triangulo antes de rotacionar ao redor de um ponto fora da origem: \n";
+    trianguloDeitado2.imprime();
+    trianguloDeitado2.rotaciona(5,0,90); // rotacionar 90° em relaçao a origem
+    cout<<"Triangulo apos rotacao: \n";
+    trianguloDeitado2.imprime();
 
 }
